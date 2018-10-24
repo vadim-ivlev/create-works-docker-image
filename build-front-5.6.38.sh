1728ed6e638f66a9dedc4155a1f685375198b2e7
ivlev <ivlev@rg.ru>
2018-10-24 17:41:03 +0300

    fix: fixed creation of inages  and starting redis

--- build-front-5.6.38.sh
+++ build-front-5.6.38.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-docker build -t rgru/works:5.6.38-front -f Dockerfile-front-5.6.38 .
-docker push rgru/works:5.6.38-front
+docker build -t rgru/works:5.6.38-front -f Dockerfile-front-5.6.38 . \
+&& docker push rgru/works:5.6.38-front
 
 
--- build-front-7.2.11.sh
+++ build-front-7.2.11.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-docker build -t rgru/works:7.2.11-front -f Dockerfile-front-7.2.11 .
-docker push rgru/works:7.2.11-front
+docker build -t rgru/works:7.2.11-front -f Dockerfile-front-7.2.11 . \
+&& docker push rgru/works:7.2.11-front
 
 
